7537208b576a750580d5afd20db9604a7dc43b3e
Rafal Konstanty <asgy23@gmail.com>
2022-04-15 11:34:49 +0200

    Adding POJO's. Closes #2.

---
+++ src/main/java/pl/konstanty/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package pl.konstanty.sfgpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/pl/konstanty/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package pl.konstanty.sfgpetclinic.model;
+
+public class Vet extends Person{
+
+}
