ef4e6465685d2c619fce0f29635a92aec8c1f705
Rafal Konstanty <asgy23@gmail.com>
2022-04-19 15:25:38 +0200

    Added services. Closes #5

---
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -0,0 +1,15 @@
+package services;
+
+import pl.konstanty.model.Pet;
+import pl.konstanty.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+}
