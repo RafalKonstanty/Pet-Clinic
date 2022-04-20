50ee9a0a45a1d8fccae16252a983a8d0f5a55716
Rafal Konstanty <asgy23@gmail.com>
2022-04-20 20:00:53 +0200

    Refactored Services, added CrudService mimicking CrudRepository. Closes #11.

---
+++ pet-clinic-data/src/main/java/services/CrudService.java
@@ -0,0 +1,20 @@
+package services;
+
+import java.util.Set;
+
+
+//Thanks to this mimicking of CrudServiceRepository interface we reduced code a lot and
+// we are not duplicating code in interfaces that share the same methods
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+
+}
--- pet-clinic-data/src/main/java/services/VetService.java
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -1,15 +1,13 @@
 package services;
 
-import pl.konstanty.model.Pet;
 import pl.konstanty.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+//    Vet findById(Long id);
+//
+//    Vet save(Vet vet);
+//
+//    Set<Vet> findAll();
 
 }
