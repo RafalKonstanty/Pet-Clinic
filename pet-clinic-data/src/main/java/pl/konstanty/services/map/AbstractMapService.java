840f889fa809dc0c61f85f201e6d1da100af27dc
Rafal Konstanty <asgy23@gmail.com>
2022-04-21 22:47:11 +0200

    Refactored Map Imp to abstract ID generation. Closes #23

--- pet-clinic-data/src/main/java/pl/konstanty/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/pl/konstanty/services/map/AbstractMapService.java
@@ -1,15 +1,14 @@
 package pl.konstanty.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import pl.konstanty.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    Set<T> findAll(){
+    protected Map<Long, T> map = new HashMap<>();
+
+    Set<T> findAll() {
         return new HashSet<>(map.values());
     }
 
@@ -17,16 +16,34 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextID());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
-    void deleteById(ID id){
+    void deleteById(ID id) {
         map.remove(id);
     }
 
-    void delete(T object){
-        map.entrySet().removeIf(e->e.getValue().equals(object));
+    void delete(T object) {
+        map.entrySet().removeIf(e -> e.getValue().equals(object));
+    }
+
+    private Long getNextID() {
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException exception){
+            System.out.println("Exception handling: There are no Id set so we will create ID nr 1");
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
