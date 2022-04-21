e159ad298400258190b6e082e38b24595be80777
Rafal Konstanty <asgy23@gmail.com>
2022-04-21 22:17:33 +0200

    Added Vets List. Closes #15

--- pet-clinic-web/src/main/java/pl/konstanty/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/konstanty/controllers/VetController.java
@@ -1,15 +1,24 @@
 package pl.konstanty.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.konstanty.services.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping
-    String listVets(){
+    String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
