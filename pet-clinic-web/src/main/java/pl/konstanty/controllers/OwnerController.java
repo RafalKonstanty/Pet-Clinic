35e7caeaabd3f1ab0633a046164115d922ec2dce
Rafal Konstanty <asgy23@gmail.com>
2022-04-21 22:10:18 +0200

    Added table on Owners Page. Closes #16

--- pet-clinic-web/src/main/java/pl/konstanty/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/konstanty/controllers/OwnerController.java
@@ -1,15 +1,24 @@
 package pl.konstanty.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.konstanty.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
