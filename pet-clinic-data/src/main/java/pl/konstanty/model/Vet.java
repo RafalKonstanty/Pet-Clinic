b69eb9073cdee2d298aae219f5cbaf3f2f5c587a
Rafal Konstanty <asgy23@gmail.com>
2022-05-05 10:43:45 +0200

    Closes #28

--- pet-clinic-data/src/main/java/pl/konstanty/model/Vet.java
+++ pet-clinic-data/src/main/java/pl/konstanty/model/Vet.java
@@ -1,5 +1,17 @@
 package pl.konstanty.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    //Specialities associate with Vet
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 
 }
---
+++ pet-clinic-data/src/main/java/pl/konstanty/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package pl.konstanty.services;
+
+import pl.konstanty.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
